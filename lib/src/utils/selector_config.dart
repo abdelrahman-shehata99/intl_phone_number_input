ad30f0c346e2e89a62fd817c9b7ad6f50dcebac6
abdelrahman-shehata99 <abdelrahman2490@gmail.com>
2026-02-26 08:35:52 +0200

    Add tablet-responsive bottom sheet support
    Add isTablet, tabletPortraitWidthFactor, tabletLandscapeWidthFactor
    to SelectorConfig. Wrap country picker bottom sheet with
    OrientationBuilder + FractionallySizedBox so it adapts width on
    tablet devices.

--- lib/src/utils/selector_config.dart
+++ lib/src/utils/selector_config.dart
@@ -36,6 +36,10 @@ class SelectorConfig {
   /// Use safe area for selectorType=BOTTOM_SHEET
   final bool useBottomSheetSafeArea;
 
+  final bool isTablet;
+  final double tabletPortraitWidthFactor;
+  final double tabletLandscapeWidthFactor;
+
   const SelectorConfig({
     this.selectorType = PhoneInputSelectorType.DROPDOWN,
     this.showFlags = true,
@@ -45,5 +49,8 @@ class SelectorConfig {
     this.leadingPadding,
     this.trailingSpace = true,
     this.useBottomSheetSafeArea = false,
+    this.isTablet = false,
+    this.tabletPortraitWidthFactor = 0.67,
+    this.tabletLandscapeWidthFactor = 0.5,
   });
 }
--- lib/src/widgets/selector_button.dart
+++ lib/src/widgets/selector_button.dart
@@ -174,63 +174,76 @@ class SelectorButton extends StatelessWidget {
               topLeft: Radius.circular(22), topRight: Radius.circular(22))),
       useSafeArea: selectorConfig.useBottomSheetSafeArea,
       builder: (BuildContext context) {
-        return Container(
-          height: MediaQuery.of(context).size.height * 0.55,
-          // constraints: BoxConstraints(
-          //   maxHeight: MediaQuery.of(context).size.height * 0.55,
-          //   minHeight: MediaQuery.of(context).size.height * 0.55,
-          // ),
-          decoration: BoxDecoration(
-            color: bottomSheetBgColor,
-          ),
-          child: Stack(
-            children: [
-              GestureDetector(
-                onTap: () => Navigator.pop(context),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).viewInsets.bottom),
-                child: DraggableScrollableSheet(
-                  initialChildSize: 1.0, // يبدأ بالحجم الكامل
-                  minChildSize: 1.0, // يمنع تصغيره عند السحب
-                  maxChildSize: 1.0, // يمنع تكبيره زيادة
-
-                  builder: (BuildContext context, ScrollController controller) {
-                    return Directionality(
-                      textDirection: Directionality.of(inheritedContext),
-                      child: Container(
-                        decoration: ShapeDecoration(
-                          color: bottomSheetBgColor,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(12),
-                              topRight: Radius.circular(12),
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            final widthFactor = selectorConfig.isTablet
+                ? (orientation == Orientation.portrait
+                    ? selectorConfig.tabletPortraitWidthFactor
+                    : selectorConfig.tabletLandscapeWidthFactor)
+                : 1.0;
+            return FractionallySizedBox(
+              widthFactor: widthFactor,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.55,
+                decoration: BoxDecoration(
+                  color: bottomSheetBgColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(22),
+                    topRight: Radius.circular(22),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    GestureDetector(
+                      onTap: () => Navigator.pop(context),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom),
+                      child: DraggableScrollableSheet(
+                        initialChildSize: 1.0,
+                        minChildSize: 1.0,
+                        maxChildSize: 1.0,
+                        builder:
+                            (BuildContext context, ScrollController controller) {
+                          return Directionality(
+                            textDirection:
+                                Directionality.of(inheritedContext),
+                            child: Container(
+                              decoration: ShapeDecoration(
+                                color: bottomSheetBgColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(12),
+                                    topRight: Radius.circular(12),
+                                  ),
+                                ),
+                              ),
+                              child: CountrySearchListWidget(
+                                countries,
+                                locale,
+                                title: title,
+                                dialCodeTextStyle: dialCodeTextStyle,
+                                countryNameTextStyle: countryNameTextStyle,
+                                titleTextStyle: titleTextStyle,
+                                searchBoxDecoration: searchBoxDecoration,
+                                scrollController: controller,
+                                showFlags: selectorConfig.showFlags,
+                                useEmoji: selectorConfig.useEmoji,
+                                autoFocus: autoFocusSearchField,
+                                verticalLineWidget: verticalDivider,
+                                closeButtonColor: closeButtonColor,
+                              ),
                             ),
-                          ),
-                        ),
-                        child: CountrySearchListWidget(
-                          countries,
-                          locale,
-                          title: title,
-                          dialCodeTextStyle: dialCodeTextStyle,
-                          countryNameTextStyle: countryNameTextStyle,
-                          titleTextStyle: titleTextStyle,
-                          searchBoxDecoration: searchBoxDecoration,
-                          scrollController: controller,
-                          showFlags: selectorConfig.showFlags,
-                          useEmoji: selectorConfig.useEmoji,
-                          autoFocus: autoFocusSearchField,
-                          verticalLineWidget: verticalDivider,
-                          closeButtonColor: closeButtonColor,
-                        ),
+                          );
+                        },
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            );
+          },
         );
       },
     );
